ac599edff527daa42441ecc7b92010e27680e24c
maxkrieg <maxwell.krieg@gmail.com>
2016-10-18 08:16:28 -0400

    Integrate google maps api

--- client/pages/Index.jsx
+++ client/pages/Index.jsx
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import Kittens from '../components/Kittens';
 import { connect } from 'react-redux';
 import { requestKittens } from '../actions/kittens';
+import styles from '../css/pages/Index.css'
 
 class Index extends Component {
+  constructor(props) {
+    super(props)
+    this.map = null
+    this.initMap = initMap.bind(this)
+  }
+
   componentDidMount() {
-    this.props.requestKittens();
+    this.initMap()
   }
 
   render() {
     return (
-      <div>
-        <Kittens />
+      <div className={styles.main}>
+        <header className={styles.header}>Header</header>
+        <div>
+          <h2 className={styles.heading}>Search</h2>
+          <input
+            className={styles.input}
+            type='text'
+            placeholder='Search here'/>
+          <div className={styles.map} ref={map => { this.mapEl = map; }}></div>
+        </div>
+        <footer className={styles.footer}>Footer</footer>
       </div>
     );
   }
+}
+
+function initMap() {
+  this.map = new google.maps.Map(this.mapEl, {
+    center: { lat: 42.1792524, lng: -71.0714453 },
+    zoom: 12
+  });
 }
 
 export default connect(() => ({}), { requestKittens })(Index)
--- client/views/index.tpl
+++ client/views/index.tpl
@@ -3,23 +3,13 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-      html, body {
-        width: 100%;
-        height: 100%;
-        margin: 0;
-      }
-      body {
-        overflow: auto;
-      }
-      .root {
-        width: 100%;
-        height: 100%;
-      }
-    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css" />
     <title><%= htmlWebpackPlugin.options.title || 'trail spotter' %></title>
+
   </head>
   <body>
     <div class="root" id="root"></div>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhwQzoWIXgTNmjrSLmfyZDl_m7KbqeLgU"
+  async defer></script>
   </body>
 </html>
